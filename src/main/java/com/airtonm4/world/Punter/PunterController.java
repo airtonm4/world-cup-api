d3c08e5bc3e28d3c8a10b261a93638c72765c3db
airtonm4 <airton.atmi@gmail.com>
2022-11-24 20:08:22 -0300

    Added all methods to punter

--- src/main/java/com/airtonm4/world/Punter/PunterController.java
+++ src/main/java/com/airtonm4/world/Punter/PunterController.java
@@ -2,8 +2,11 @@ package com.airtonm4.world.Punter;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,12 +20,40 @@ public class PunterController {
     }
 
     @GetMapping("/api/punter")
-    List<Punter> all() {
+    List<Punter> allPunter() {
         return repository.findAll();
     }
 
     @PostMapping("/api/punter")
     Punter newPunter(@RequestBody Punter newPunter) {
         return this.repository.save(newPunter);
+    }
+
+    @GetMapping("/api/punter/{id}")
+    Punter onePunter(@PathVariable Long id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    @DeleteMapping("/api/punter/{id}")
+    void deletePunter(@PathVariable Long id) {
+        repository.deleteById(id);
+    }
+
+    @DeleteMapping("/api/punter/all")
+    void deleteAllPunters() {
+        repository.deleteAll();
+    }
+
+    @PutMapping("/api/punter/")
+    Punter replacePunter(@RequestBody Punter newPunter, @PathVariable Long id) {
+        return repository.findById(id)
+                .map(punter -> {
+                    punter.setName(newPunter.getName());
+                    return repository.save(punter);
+                })
+                .orElseGet(() -> {
+                    newPunter.setId(id);
+                    return repository.save(newPunter);
+                });
     }
 }
